912f0ea120e6358a9a3706217fd4e298c976780f
callum-i <callum.iddon@stfc.ac.uk>
2017-04-18 14:29:32 +0100

    Added comments
    Using this link,
    https://kernel.googlesource.com/pub/scm/bluetooth/bluez/+/5.16/doc/agent-api.txt
    Copied in the relevant comments to explain what each method does.

--- Interfaces/RPI_to_Arduino/piduino/autoPair.py
+++ Interfaces/RPI_to_Arduino/piduino/autoPair.py
@@ -38,6 +38,12 @@ class Agent(dbus.service.Object):
 
     @dbus.service.method(AGENT_INTERFACE, in_signature="", out_signature="")
     def Release(self):
+        '''
+        This method gets called when the service daemon unregisters the agent.
+        An agent can use it to do cleanup tasks. There is no need to unregister
+        the agent, because when this method gets called it has already been
+        unregistered.
+        '''
         print("Release")
         if self.exit_on_release:
             mainloop.quit()
@@ -45,6 +51,13 @@ class Agent(dbus.service.Object):
     # In object, out string
     @dbus.service.method(AGENT_INTERFACE, in_signature="o", out_signature="s")
     def RequestPinCode(self, device):
+        '''
+        This method gets called when the service daemon needs to get the
+        passkey for an authentication. The return value should be a string of
+        1-16 characters length. The string can be alphanumeric.
+        Possible errors: org.bluez.Error.Rejected
+                         org.bluez.Error.Canceled
+        '''
         print("RequestPinCode (%s)" % (device))
         set_trusted(device)
         return raw_input("PIN")
@@ -52,17 +65,54 @@ class Agent(dbus.service.Object):
     # In object 32bituint 16bituint
     @dbus.service.method(AGENT_INTERFACE, in_signature="ouq", out_signature="")
     def DisplayPasskey(self, device, passkey, entered):
+        '''
+        This method gets called when the service daemon needs to display a
+        passkey for an authentication. The entered parameter indicates the
+        number of already typed keys on the remote side. An empty reply should
+        be returned. When the passkey needs no longer to be displayed, the
+        Cancel method of the agent will be called. During the pairing process
+        this method might be called multiple times to update the entered value.
+
+        Note that the passkey will always be a 6-digit number, so the display
+        should be zero-padded at the start if the value contains less than 6
+        digits.
+        '''
         print("DisplayPasskey (%s, %06u entered %u)" % (device, passkey,
                                                         entered))
 
     # In object string
     @dbus.service.method(AGENT_INTERFACE, in_signature="os", out_signature="")
     def DisplayPinCode(self, device, pincode):
+        '''
+        This method gets called when the service daemon needs to display a
+        pincode for an authentication. An empty reply should be returned. When
+        the pincode needs no longer to be displayed, the Cancel method of the
+        agent will be called. This is used during the pairing process of
+        keyboards that don't support Bluetooth 2.1 Secure Simple Pairing, in
+        contrast to DisplayPasskey which is used for those that do. This
+        method will only ever be called once since older keyboards do not
+        support typing notification. Note that the PIN will always be a
+        6-digit number, zero-padded to 6 digits. This is for harmony with the
+        later specification.
+
+        Possible errors: org.bluez.Error.Rejected
+                         org.bluez.Error.Canceled
+        '''
         print("DisplayPinCode (%s, %s)" % (device, pincode))
 
     # In object 32bituint
     @dbus.service.method(AGENT_INTERFACE, in_signature="ou", out_signature="")
     def RequestConfirmation(self, device, passkey):
+        '''
+        This method gets called when the service daemon needs to confirm a
+        passkey for an authentication. To confirm the value it should return
+        an empty reply or an error in case the passkey is invalid. Note that
+        the passkey will always be a 6-digit number, so the display should be
+        zero-padded at the start if the value contains less than 6 digits.
+
+        Possible errors: org.bluez.Error.Rejected
+                         org.bluez.Error.Canceled
+        '''
         print("RequestConfirmation (%s, %06d)" % (device, passkey))
         confirm = ask("Confirm passkey (yes/no): ")
         if (confirm == "yes"):
@@ -73,6 +123,14 @@ class Agent(dbus.service.Object):
     # In object
     @dbus.service.method(AGENT_INTERFACE, in_signature="o", out_signature="")
     def RequestAuthorization(self, device):
+        '''
+        This method gets called to request the user to authorize an incoming
+        pairing attempt which would in other circumstances trigger the
+        just-works model.
+
+        Possible errors: org.bluez.Error.Rejected
+                         org.bluez.Error.Canceled
+        '''
         print("RequestAuthorization (%s)" % (device))
         auth = ask("Authorize? (yes/no): ")
         if (auth == "yes"):
@@ -81,6 +139,10 @@ class Agent(dbus.service.Object):
 
     @dbus.service.method(AGENT_INTERFACE, in_signature="", out_signature="")
     def Cancel(self):
+        '''
+        This method gets called to indicate that the agent request failed
+        before a reply was returned.
+        '''
         print("Cancel")
 
 
